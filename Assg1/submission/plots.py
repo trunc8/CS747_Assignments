4e1a2ae6d36cbe13dee66835d18e0bb019b0e8cd
Siddharth Saha <sahasiddharth611@gmail.com>
2020-09-22 15:08:57 +0530

    Added python script to plot regrets; can be used for both T1 and T2

---
+++ Assg1/submission/plots.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# trunc8 did this
+
+import matplotlib.pyplot as plt
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n")
+args = parser.parse_args()
+f = open(f"outputDataT{args.n}.txt", "r")
+
+instances = ["../instances/i-1.txt", "../instances/i-2.txt", "../instances/i-3.txt"]
+if n == 1:
+    algorithms = ["epsilon-greedy", "ucb", "kl-ucb", "thompson-sampling"]
+elif n == 2:
+    algorithms = ["thompson-sampling", "thompson-sampling-with-hint"]
+horizons = [100, 400, 1600, 6400, 25600, 102400]
+
+Expt = {}
+for instance in instances:
+    Expt[instance] = {}
+    for algorithm in algorithms:
+        Expt[instance][algorithm] = {}
+        for horizon in horizons:
+            Expt[instance][algorithm][horizon] = 0
+for line in f:
+    instance, algorithm, _, _, horizon, REG = line.split(", ")
+    Expt[instance][algorithm][int(horizon)] += float(REG)/50
+
+for instance in instances:
+    plt.figure()
+    for algorithm in algorithms:
+        lists = sorted(Expt[instance][algorithm].items())
+        x, y = zip(*lists)
+        plt.plot(x, y, label = algorithm)
+    plt.xscale("log")
+    plt.xlabel("Horizon")
+    plt.ylabel("Cumulative Expected Regret")
+    plt.title(f"{instance}")
+    plt.legend()
+    plt.show()
+
+f.close()
