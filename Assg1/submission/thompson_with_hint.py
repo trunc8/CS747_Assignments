6eda1e49cdd77b65bc6b4f5d52d70dbe8b821030
Siddharth Saha <sahasiddharth611@gmail.com>
2020-09-25 12:48:40 +0530

    Replicated no_hint code here again, added sorted rew_means(hint), have put weird conditions to use hint. That will change

--- Assg1/submission/thompson_with_hint.py
+++ Assg1/submission/thompson_with_hint.py
@@ -10,11 +10,14 @@ def pull_arm(reward_means, arm):
     '''
     return (random.uniform(0,1) < reward_means[arm])
 
-def soln(reward_means, horizon, num_of_arms):
+def soln(reward_means, horizon, num_of_arms, hint_ls):
     '''
+    Here the parameter reward_means is in sorted order to be used to gain some hint.
     If multiple arms have highest empirical mean, use the index returned by argmax
     No Round Robin exploration
     '''
+    hint = np.min(hint_ls)
+    # hint = 0
     beta_samples = np.empty(num_of_arms)
     arm_successes = np.zeros(num_of_arms)
     arm_fails = np.zeros(num_of_arms)
@@ -22,12 +25,15 @@ def soln(reward_means, horizon, num_of_arms):
     REG = 0
     for t in range(1, horizon+1):
         for a in range(num_of_arms):
+            if t > horizon*0.75 and arm_successes[a]/(arm_successes[a]+arm_fails[a]+1) < hint and random.uniform(0,1) < 0.5:
+              beta_samples[a] = 0
+              continue
             beta_samples[a] = random.betavariate(arm_successes[a]+1, arm_fails[a]+1)
         arm = np.argmax(beta_samples)
         if pull_arm(reward_means, arm) == 1:
-            arm_successes[a] += 1
+            arm_successes[arm] += 1
             REW += 1
         else:
-            arm_fails[a] += 1
+            arm_fails[arm] += 1
     REG = horizon*max(reward_means) - REW
     return REG
